f8cc1e2e9e18252ee5806a3a8a723f4a1946a328
Maria Eduarda <e107ti@etec.sp.gov.br>
2024-04-18 19:36:29 -0300

    exercício resolvido

---
+++ script.js
@@ -0,0 +1,11 @@
+alert ("Olá Usuário");
+
+let nome = prompt("Digite seu nome?");
+
+let sobrenome = prompt("Digite seu sobrenome?");
+
+alert("Nome Completo");
+alert(nome + " " + sobrenome);
+
+alert("Nome de Catálogo")
+alert(sobrenome.toUpperCase() + ", " + nome);
